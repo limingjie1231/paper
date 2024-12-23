fed51c421b861c45a7ee281f180c1c491fc6e6d7
limingjie1231 <limingjie1231ly@gmail.com>
2024-12-23 10:50:16 +0000

    1

--- network/arch_2dpass.py
+++ network/arch_2dpass.py
@@ -8,58 +8,7 @@ from network.basic_block import Lovasz_loss
 from network.baseline import get_model as SPVCNN
 from network.base_model import LightningBaseModel
 from network.basic_block import ResNetFCN
-class Attention(nn.Module):
-    def __init__(self, channels):
-        super(Attention, self).__init__()
-        self.pseudo_in, self.valid_in = channels
-        middle = self.valid_in // 4
-        self.fc1 = nn.Linear(self.pseudo_in, middle)
-        self.fc2 = nn.Linear(self.valid_in, middle)
-        self.fc3 = nn.Linear(2*middle, 2)
-        self.conv1 = nn.Sequential(nn.Conv1d(self.pseudo_in, self.valid_in, 1),
-                                    nn.BatchNorm1d(self.valid_in),
-                                    nn.ReLU())
-        self.conv2 = nn.Sequential(nn.Conv1d(self.valid_in, self.valid_in, 1),
-                                    nn.BatchNorm1d(self.valid_in),
-                                    nn.ReLU())
 
-    def forward(self, pseudo_feas, valid_feas):
-        # pfeas = []
-        # ifeas = []
-        # leng = 0
-        # for i in range(len(list)):
-        #     if i == 0:
-        #         pfeas.append(pseudo_feas[leng:(leng + list[i])])
-        #         ifeas.append(valid_feas[leng:(leng + list[i])])
-        #         leng += list[i]
-        #     else:
-        #         pfeas.append(pseudo_feas[leng:(leng + list[i])])
-        #         ifeas.append(valid_feas[leng:(leng + list[i])])
-        #         leng += list[i]
-        pseudo_feas = torch.unsqueeze(pseudo_feas, dim=0).permute(0,2,1)
-        valid_feas = torch.unsqueeze(valid_feas, dim=0).permute(0,2,1)
-        batch = pseudo_feas.size(0)
-
-        pseudo_feas_f = pseudo_feas.transpose(1,2).contiguous().view(-1, self.pseudo_in)
-        valid_feas_f = valid_feas.transpose(1,2).contiguous().view(-1, self.valid_in)
-
-        pseudo_feas_f_ = self.fc1(pseudo_feas_f)
-        valid_feas_f_ = self.fc2(valid_feas_f)
-        pseudo_valid_feas_f = torch.cat([pseudo_feas_f_, valid_feas_f_],dim=-1)
-        weight = torch.sigmoid(self.fc3(pseudo_valid_feas_f))
-
-        pseudo_weight = weight[:,0].squeeze()
-        pseudo_weight = pseudo_weight.view(batch, 1, -1)
-
-        valid_weight = weight[:,1].squeeze()
-        valid_weight = valid_weight.view(batch, 1, -1)
-
-        pseudo_features_att = self.conv1(pseudo_feas)  * pseudo_weight
-        valid_features_att     =  self.conv2(valid_feas)      *  valid_weight
-        pseudo_features_att = pseudo_features_att.squeeze(dim=0).permute(1, 0)
-        valid_features_att = valid_features_att.squeeze(dim=0).permute(1, 0)
-        features_att =pseudo_features_att + valid_features_att
-        return features_att
 class xModalKD(nn.Module):
     def __init__(self,config):
         super(xModalKD, self).__init__()
@@ -107,7 +56,7 @@ class xModalKD(nn.Module):
             seg_labelweights = torch.Tensor(np.power(np.amax(seg_labelweights) / seg_labelweights, 1 / 3.0))
         else:
             seg_labelweights = None
-        self.attention = Attention(channels=[64, 64])
+
         self.ce_loss = nn.CrossEntropyLoss(weight=seg_labelweights, ignore_index=config['dataset_params']['ignore_label'])
         self.lovasz_loss = Lovasz_loss(ignore=config['dataset_params']['ignore_label'])
 
@@ -132,42 +81,54 @@ class xModalKD(nn.Module):
         lovasz_loss = self.lovasz_loss(F.softmax(logits, dim=1), labels)
         return ce_loss + lovasz_loss
 
+    def fusion_to_single_KD(self, data_dict, idx):
+        batch_idx = data_dict['batch_idx']
+        point2img_index = data_dict['point2img_index']
+        last_scale = self.scale_list[idx - 1] if idx > 0 else 1
+        img_feat = data_dict['img_scale{}'.format(self.scale_list[idx])]
+        pts_feat = data_dict['layer_{}'.format(idx)]['pts_feat']
+        coors_inv = data_dict['scale_{}'.format(last_scale)]['coors_inv']
+
+        # 3D prediction
+        pts_pred_full = self.multihead_3d_classifier[idx](pts_feat)
+
+        # correspondence
+        pts_label_full = self.voxelize_labels(data_dict['labels'], data_dict['layer_{}'.format(idx)]['full_coors'])
+        pts_feat = self.p2img_mapping(pts_feat[coors_inv], point2img_index, batch_idx)
+        pts_pred = self.p2img_mapping(pts_pred_full[coors_inv], point2img_index, batch_idx)
+
+        # modality fusion
+        feat_learner = F.relu(self.leaners[idx](pts_feat))
+        feat_cat = torch.cat([img_feat, feat_learner], 1)
+        feat_cat = self.fcs1[idx](feat_cat)
+        feat_weight = torch.sigmoid(self.fcs2[idx](feat_cat))
+        fuse_feat = F.relu(feat_cat * feat_weight)
+
+        # fusion prediction
+        fuse_pred = self.multihead_fuse_classifier[idx](fuse_feat)
+
+        # Segmentation Loss
+        seg_loss_3d = self.seg_loss(pts_pred_full, pts_label_full)
+        seg_loss_2d = self.seg_loss(fuse_pred, data_dict['img_label'])
+        loss = seg_loss_3d + seg_loss_2d * self.lambda_seg2d / self.num_scales
+
+        # KL divergence
+        xm_loss = F.kl_div(
+            F.log_softmax(pts_pred, dim=1),
+            F.softmax(fuse_pred.detach(), dim=1),
+        )
+        loss += xm_loss * self.lambda_xm / self.num_scales
+
+        return loss, fuse_feat
+
     def forward(self, data_dict):
         loss = 0
         img_seg_feat = []
-        batch_idx = data_dict['batch_idx']
-        point2img_index = data_dict['point2img_index']
 
-        for idx, scale in enumerate(self.scale_list):
-            img_feat = data_dict['img_scale{}'.format(scale)]
-            pts_feat = data_dict['layer_{}'.format(idx)]['pts_feat']
-            coors_inv = data_dict['scale_{}'.format(scale)]['coors_inv']
-
-            # 3D prediction
-            pts_pred_full = self.multihead_3d_classifier[idx](pts_feat)
-
-            # correspondence
-            pts_label_full = self.voxelize_labels(data_dict['labels'], data_dict['layer_{}'.format(idx)]['full_coors'])
-            pts_feat = self.p2img_mapping(pts_feat[coors_inv], point2img_index, batch_idx)
-            pts_pred = self.p2img_mapping(pts_pred_full[coors_inv], point2img_index, batch_idx)
-
-            fuse_pred = self.attention(pts_feat, img_feat)
-
-
-            img_seg_feat.append(fuse_pred)
-            fuse_pred = self.multihead_fuse_classifier[idx](fuse_pred)
-
-            # Segmentation Loss
-            seg_loss_3d = self.seg_loss(pts_pred_full, pts_label_full)
-            seg_loss_2d = self.seg_loss(fuse_pred, data_dict['img_label'])
-            loss += seg_loss_3d + seg_loss_2d * self.lambda_seg2d / self.num_scales
-
-            # KL divergence
-            xm_loss = F.kl_div(
-                F.log_softmax(pts_pred, dim=1),
-                F.softmax(fuse_pred, dim=1),
-            )
-            loss += xm_loss * self.lambda_xm / self.num_scales
+        for idx in range(self.num_scales):
+            singlescale_loss, fuse_feat = self.fusion_to_single_KD(data_dict, idx)
+            img_seg_feat.append(fuse_feat)
+            loss += singlescale_loss
 
         img_seg_logits = self.classifier(torch.cat(img_seg_feat, 1))
         loss += self.seg_loss(img_seg_logits, data_dict['img_label'])
